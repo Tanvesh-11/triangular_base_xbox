fa8d2eadae6a18450a5cde71f9eb084b9a97de42
Tanvesh <54196212+Tanvesh-11@users.noreply.github.com>
2019-08-17 12:54:41 +0530

    Slave- Address 10

---
+++ I2C_slave_address-10.ino
@@ -0,0 +1,156 @@
+
+
+#include<I2C_Anything.h>
+#include<Wire.h>    
+const byte MY_address=10;
+
+const int encoder1=2;
+volatile long  count = 0;
+long realCount ;
+
+unsigned long lastRead = 0;
+int a;
+int pc;
+const int in1=8;
+const int in2=12;
+float pwmpin=10;
+float error=0;
+float previouserror=0;
+float ierror=0;
+float derror=0;
+float kp=0.00545 , KP=0; 
+float ki=0.0099, KI=0;
+float kd=0.01 , KD=0;
+long targetcount;
+float output;
+int pwm=0;
+
+
+
+
+void setup(){
+ // put your setup code here, to run once:
+ 
+  Wire.begin(MY_address);
+  
+  Serial.begin(9600);
+  Serial.println("start...");
+  
+  pinMode(encoder1,INPUT_PULLUP);
+  attachInterrupt(0, isrCount, CHANGE); 
+  a= digitalRead(encoder1);
+  Wire.onReceive(event);
+  
+}   
+
+void loop(){
+  // put your main code here, to run repeatedly:
+  analogWrite(pwmpin,pwm);
+  
+  
+
+ if (millis() - lastRead >= 100)
+  {
+    lastRead  = millis();
+ 
+    //noInterrupts();
+    realCount = count;
+     
+    count = 0;
+    //interrupts();
+  PID(); 
+  } 
+   
+   Serial.print(targetcount);
+  Serial.print("        ");
+  Serial.print(realCount);
+  Serial.print("                 ");
+  Serial.println(pwm); 
+
+    
+}
+
+void isrCount()
+{
+
+  count++;
+ }
+
+void event()
+{ 
+  I2C_readAnything(pc);
+  
+ // Serial.println(pc);
+    targetcount=abs(pc);
+
+  if(pc>0)
+  {
+    digitalWrite(in1,HIGH);
+    digitalWrite(in2,LOW);
+  }
+
+  if(pc<0)
+  {
+    digitalWrite(in1,LOW);
+    digitalWrite(in2,HIGH);
+  }
+  
+
+   
+}
+void PID()
+{
+  error=targetcount-realCount;
+  ierror=ierror+error;
+  derror=(error-previouserror);
+    
+
+//  if(targetcount>=4000)
+//   {
+// if (count==0)
+//  {
+//  ierror=0;
+// }
+//   }
+  if(targetcount==0 || targetcount==1 )
+ {
+ 
+  KP=0;
+  KD=0;
+  KI=0;
+  ierror=0;
+  analogWrite(pwmpin,0);
+ }
+ else
+ {
+  KP=kp;
+  KD=kd;
+  KI=ki;
+ }
+ 
+  output=error*KP+ ierror*KI +derror*KD;
+
+  if(output>255)
+  {
+   pwm=255;
+  }
+
+  else if(output<=0)
+  {
+    pwm=0;
+  }
+
+  else
+  {
+    pwm=output;
+  }
+  
+
+   previouserror=error;
+
+   analogWrite(pwmpin,pwm);
+
+}
+
+
+
